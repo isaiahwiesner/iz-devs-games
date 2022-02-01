db2b1735ec4e923764281281bf23042a60385776
_iz_ <62268447+izzzzthewiz@users.noreply.github.com>
2022-02-01 16:26:00 -0500

    idfk

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-function addBox(title, link, desc){
-    document.write("<div class='box'><h1><a href='" + link + "'>" + title + "</h1><p>" + desc + "</p></div>")
+function addBox(title, link, desc, img){
+    document.write("<div class='box'><div class='text'><h1><a href='" + link + "'>" + title + "</h1></a><p>" + desc + "</p></div><img src='" + img + "'>" + "</div>")
 }
