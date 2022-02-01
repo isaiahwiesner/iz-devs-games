3c2b50f2ae4e4c7a0b08cc902345768e2b3bdd32
_iz_ <62268447+izzzzthewiz@users.noreply.github.com>
2022-02-01 16:39:55 -0500

    edit

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 function addBox(title, link, desc, img){
-    document.write("<div class='box'><div class='text'><h1><a href='" + link + "'>" + title + "</h1></a><p>" + desc + "</p></div><img src='" + img + "'>" + "</div>")
+    document.write("<div class='box'><div class='text'><h1><a href='" + link + "'>" + title + "</h1></a><p>" + desc + "</p></div><div class='img'><img src='" + img + "'>" + "</div></div>")
 }
