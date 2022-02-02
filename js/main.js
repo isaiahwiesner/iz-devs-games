412ca55cfa58de66c2229addf16ff9fbd311d6c5
_iz_ <62268447+izzzzthewiz@users.noreply.github.com>
2022-02-01 19:57:11 -0500

    efadmkal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
-function addBox(title, link, desc, img){
-    document.write("<div class='box'><div class='text'><h1><a href='" + link + "'>" + title + "</h1></a><p>" + desc + "</p></div><div class='img'><img src='" + img + "'>" + "</div></div>")
+const gameList = document.getElementById('gameList');
+
+const loadGames = async () => {
+    try {
+        const res = await fetch("https://play.izzdevs.me/api/games.json");
+        let izgames = await res.json();
+        displayGames(izgames);
+        console.log(izgames);
+    } catch(err) {
+        console.log(err);
+    }
 }
+
+const displayGames = (games) => {
+    const htmlString = games
+        .map((game) => {
+            return `
+                <div class="box">
+                    <div class="text">
+                        <h1><a href="${game.link}">${game.name}</a></h1>
+                        <p>${game.description}</p>
+                    </div>
+                    <div class="img">
+                        <img src="${game.image}">
+                    </div>
+                </div>
+            `;
+        })
+        .join('');
+    gameList.innerHTML = htmlString;
+}
+
+loadGames();
