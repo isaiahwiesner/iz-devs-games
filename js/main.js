b0e1fb8fdaf62c60c4407897ae3a5a916e764dae
_iz_ <62268447+izzzzthewiz@users.noreply.github.com>
2022-02-01 21:01:26 -0500

    Searchbar complete

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
+const gameList = document.getElementById('gameList');
+const searchbar = document.getElementById('searchbar');
+
 const loadGames = async () => {
+    const res = await fetch("https://play.izzdevs.me/js/api/games.json");
+    let izgames = await res.json();
+    displayGames(izgames);
+    console.log(izgames);
     try {
-        const res = await fetch("https://play.izzdevs.me/js/api/games.json");
-        let izgames = await res.json();
-        displayGames(izgames);
-        console.log(izgames);
     } catch(err) {
         console.log(err);
     }
@@ -11,21 +14,43 @@ const loadGames = async () => {
 
 const displayGames = (games) => {
     const htmlString = games
-        .map((game) => {
-            return `
-                <div class="box">
-                    <div class="text">
-                        <h1><a href="${game.link}">${game.name}</a></h1>
-                        <p>${game.description}</p>
-                    </div>
-                    <div class="img">
-                        <img src="${game.image}">
-                    </div>
-                </div>
-            `;
-        })
-        .join('');
-    document.getElementById("gameList").innerHTML = htmlString;
+    .map((game) => {
+        return `
+        <div class="box">
+        <div class="text">
+        <h1><a href="${game.link}">${game.name}</a></h1>
+        <p>${game.description}</p>
+        </div>
+        <div class="img">
+        <img src="${game.image}">
+        </div>
+        </div>
+        `;
+    })
+    .join('');
+    gameList.innerHTML = htmlString;
 }
 
+async function search(searchString) {
+    try {
+        const res = await fetch("https://play.izzdevs.me/js/api/games.json");
+        let izgames = await res.json();
+        const filteredGames = izgames.filter((game) => {
+            return (
+                game.name.toLowerCase().includes(searchString)
+            );
+        });
+        displayGames(filteredGames);
+        if (filteredGames.toString() == ""){
+            gameList.innerHTML = `<h1 class="center error">No games found!</h1>`;
+        }
+    } catch(err) {
+        console.log(err);
+    }
+}
+
+searchbar.addEventListener("keyup", (e) => {
+    search(e.target.value)
+});
+    
 loadGames();
